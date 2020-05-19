9e94e4d2bd63c2569d48859de0e36c94b8124fea
marcoscerioni <marcoscerioni98@gmail.com>
2020-05-19 15:14:49 -0300

    titulo, icono, y cierra bien apretando la cruz

--- juegoVida.py
+++ juegoVida.py
@@ -1,9 +1,17 @@
-import pygame
 import numpy as np
-import time
+import time, os, pygame
+import sys
 
 
 pygame.init()
+pygame.display.set_caption("Juego de la vida - Marcos Cerioni")
+
+iconPath = "./icono.ico" 
+if os.path.exists(iconPath):
+    icono = pygame.image.load(iconPath)
+    pygame.display.set_icon(icono)
+
+
 
 alto, ancho  = 1000, 1000  # Ancho y largo de 1000 x 1000 pixeles
 screen = pygame.display.set_mode((alto, ancho)) # Creo la pantalla del juego
@@ -33,60 +41,89 @@ gameState[20, 23] = 1
 
 pausarEjec = False # Control ejecucion.
 
-# Bucle de Ejecucion
+
+iteraciones = 0 # Acumulador de cantidad de iteraciones:
+
+
 while True:
-	newGameState = np.copy(gameState) # Realizo una copia del estado actual del juego. 
+    newGameState = np.copy(gameState) # Realizo una copia del estado actual del juego. 
 
-	screen.fill(bg) # Limpio info para que no se superponga
-	time.sleep(0.1) 
+    screen.fill(bg) # Limpio info para que no se superponga
+    time.sleep(0.1) 
 
-	ev = pygame.event.get() # Registro eventos del teclado y mouse. 
+    ev = pygame.event.get() # Registro eventos del teclado y mouse. 
 
-	for event in ev:
-		if event.type == pygame.KEYDOWN:
-			pausarEjec = not pausarEjec
+    # Contador de población
+    poblacion = 0
 
-		mouseClick = pygame.mouse.get_pressed()
-		if sum(mouseClick) > 0:
-			posX, posY = pygame.mouse.get_pos()
-			celX, celY = int(np.floor(posX / anchoX)), int(np.floor(posY / altoY))
-			newGameState[celX, celY] = not mouseClick[2]
+    for event in ev:
+        if event.type == pygame.QUIT:
+           pygame.quit()
+           sys.exit()
 
-	# Dos for que recorran el ejeX y el ejeY para cada celda generada
-	for y in range(0, celdasX):
-		for x in range(0, celdasY):
+        if event.type == pygame.KEYDOWN:
+            pausarEjec = not pausarEjec
 
-			if not pausarEjec:
-				# Nº de vecinos cercanos. 
-				vecinos = gameState[(x-1) % celdasX, (y-1)  % celdasY] + \
-						  gameState[(x)   % celdasX, (y-1)  % celdasY] + \
-						  gameState[(x+1) % celdasX, (y-1)  % celdasY] + \
-						  gameState[(x-1) % celdasX, (y)    % celdasY] + \
-						  gameState[(x+1) % celdasX, (y)    % celdasY] + \
-						  gameState[(x-1) % celdasX, (y+1)  % celdasY] + \
-						  gameState[(x)   % celdasX, (y+1)  % celdasY] + \
-						  gameState[(x+1) % celdasX, (y+1)  % celdasY]
+        mouseClick = pygame.mouse.get_pressed()
+        if sum(mouseClick) > 0:
+            posX, posY = pygame.mouse.get_pos()
+            celX, celY = int(np.floor(posX / anchoX)), int(np.floor(posY / altoY))
+            newGameState[celX, celY] = not mouseClick[2]
 
-				# Regla 1: Una celula muerta con 3 vecinos vivos, revive. 
-				if gameState[x, y] == 0 and vecinos == 3:
-					newGameState[x, y] = 1
-				# Regla 2: Celula viva con menos de 2 o mas de 3 vecinos vivos, muere. 
-				elif gameState[x, y] == 1 and (vecinos < 2 or vecinos > 3):
-					newGameState[x, y] = 0
+    if not pausarEjec:
+        iteraciones += 1
+    
+    # Dos for que recorran el ejeX y el ejeY para cada celda generada
+    for y in range(0, celdasX):
+        for x in range(0, celdasY):
+
+            if not pausarEjec:
+                
+                # Incremento el contador de población:
+                if gameState[x, y] == 1:
+                    poblacion += 1
+                
+                # Nº de vecinos cercanos. 
+                vecinos = gameState[(x-1) % celdasX, (y-1)  % celdasY] + \
+                          gameState[(x)   % celdasX, (y-1)  % celdasY] + \
+                          gameState[(x+1) % celdasX, (y-1)  % celdasY] + \
+                          gameState[(x-1) % celdasX, (y)    % celdasY] + \
+                          gameState[(x+1) % celdasX, (y)    % celdasY] + \
+                          gameState[(x-1) % celdasX, (y+1)  % celdasY] + \
+                          gameState[(x)   % celdasX, (y+1)  % celdasY] + \
+                          gameState[(x+1) % celdasX, (y+1)  % celdasY]
+
+                # Regla 1: Una celula muerta con 3 vecinos vivos, revive. 
+                if gameState[x, y] == 0 and vecinos == 3:
+                    newGameState[x, y] = 1
+                # Regla 2: Celula viva con menos de 2 o mas de 3 vecinos vivos, muere. 
+                elif gameState[x, y] == 1 and (vecinos < 2 or vecinos > 3):
+                    newGameState[x, y] = 0
 
 
-			# Coordenadas de los rectangulos
-			poly = [((x)   * anchoX,   y     * altoY),
-					((x+1) * anchoX,   y     * altoY),
-					((x+1) * anchoX,   (y+1) * altoY),
-					((x)   * anchoX,   (y+1) * altoY)]
-			
-			if newGameState[x, y] == 0:
-				pygame.draw.polygon(screen, (128, 128, 128), poly, 1)
-			else:
-				pygame.draw.polygon(screen, (255, 255, 255), poly, 0) 
+            # Coordenadas de los rectangulos
+            poly = [((x)   * anchoX,   y     * altoY),
+                    ((x+1) * anchoX,   y     * altoY),
+                    ((x+1) * anchoX,   (y+1) * altoY),
+                    ((x)   * anchoX,   (y+1) * altoY)]
+            
+            if newGameState[x, y] == 0:
+                pygame.draw.polygon(screen, (128, 128, 128), poly, 1)
+            else:
+                if pausarEjec:
+                    pygame.draw.polygon(screen, (128, 128, 128), poly, 0)
+                else:
+                    pygame.draw.polygon(screen, (255, 255, 255), poly, 0) 
 
-	# Actualizo el estado del juego
-	gameState = np.copy(newGameState)
 
-	pygame.display.flip() # Mostrando y actuliazando los fotogramas de la escena en c/iteracion del bucle
+    title = f"Juego de la vida - Marcos Cerioni - Población: {poblacion} - Generación: {iteraciones}"
+    if pausarEjec:
+        title += " - [PAUSADO]"
+    pygame.display.set_caption(title)
+
+
+    # Actualizo el estado del juego
+    gameState = np.copy(newGameState)
+
+    pygame.display.flip() # Mostrando y actuliazando los fotogramas de la escena en c/iteracion del bucle
+
